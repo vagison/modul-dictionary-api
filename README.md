701331ef36855f385ade3c0e377dfca541b7596c
unknown <vagison@gmail.com>
2022-02-03 12:56:56 +0400

    Updating readme file and add data to the project

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ Final project for Modul Technical Dictionary API
 ## About the project
 This English-Armenian / Armenian-English online technical dictionary includes more than 5000 entries (including: words, phrases and abbreviations) that are widely used in various technical fields. The database is created based on our company's years of experience in the field of technical documentation and translation.
 
+## About requirements for the project
+1. The DBMS used for the project is MySQL. You need to install it if don't currently have it
+2. Use your credentials to connect to the database and then create a scheme with the name `modul-dict`
+
 ## To run this project:
 1. Clone this repo
-2. Run `npm install`
-3. Run `npm start`
+2. Change the `database.js` file with your credentials to connect to the desired database where you have already created `modul-dict` scheme
+3. Import the data into selected scheme from `data` folder of this project
+4. Run `npm install`
+5. Run `npm start`
---
+++ data/modul-dict_examples.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: modul-dict
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `examples`
+--
+
+DROP TABLE IF EXISTS `examples`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `examples` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `englishExample` varchar(255) NOT NULL,
+  `armenianExample` varchar(255) NOT NULL,
+  `translationId` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `translationId` (`translationId`),
+  CONSTRAINT `examples_ibfk_1` FOREIGN KEY (`translationId`) REFERENCES `translations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=313 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `examples`
+--
+
+LOCK TABLES `examples` WRITE;
+/*!40000 ALTER TABLE `examples` DISABLE KEYS */;
+INSERT INTO `examples` VALUES (84,'Force brings life to right','Ուժն է ծնում իրավունք',105),(102,'my computer','իմ համակարգիչ',102),(265,'Citizen of RA.','ՀՀ քաղաքացի։',308),(269,'Beam for the building.','Շինության հեծան:',95),(270,'gravel bed','կոպճային բարձիկ',637),(273,'Timber can be used as a construction material.','Փայտանյութն օգտագործվում է որպես շինանյութ:',98),(275,'The metre is a unit of length in the International System of Units.','Երկարության չափման միավոր է միավորների մետրական և միջազգային համակարգերում:',760),(307,'Annual marathon.','Ամենամյա մարաթոն։',38),(308,'Аnother marathon.','Այլ մարաթոն։',38),(309,'Apple is a fruit.','Խնձորը միրգ է:',90),(310,'Water (chemical formula H2O) is an inorganic, transparent, tasteless, odorless, and nearly colorless chemical substance, which is the main constituent of Earth\'s hydrosphere and the fluids of all known living organisms (in which it acts as a solvent).','Ջուր, անօրգանական միացություն, ջրածնի օքսիդ, քիմիական բանաձևը՝ H2O։ Ջուրը բնության ամենատարածված նյութն է, բնության մեջ հանդիպում է պինդ, հեղուկ, գազային վիճակներում։ Կազմում է կենդանի օրգանիզմների բաղադրության 2/3 մասը։',100),(311,'dummy data','',93),(312,'dummy data','',758);
+/*!40000 ALTER TABLE `examples` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-03 11:50:50
