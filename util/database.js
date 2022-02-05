ce8b7bc2bb1d5a25b029fb49120c57fb63b7991c
unknown <vagison@gmail.com>
2022-02-05 21:54:27 +0400

    Updating

--- util/database.js
+++ util/database.js
@@ -11,3 +11,19 @@ const db = new Sequelize("modul-dict", "root", "admin", {
 
 // --- Exporting the database connection
 module.exports = db;
+
+
+// // --- Importing required packages
+
+// // importing ORM packages
+// const { Sequelize } = require("sequelize");
+
+// // --- Creating the database connection
+// const db = new Sequelize("modul-dict", "postgres", "test", {
+//   dialect: "postgres",
+//   host: "localhost",
+// });
+
+// // --- Exporting the database connection
+// module.exports = db;
+
