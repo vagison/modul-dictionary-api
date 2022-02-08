915714a8dfcdd7b96af1066bf043fba2e0dd084b
unknown <vagison@gmail.com>
2022-02-08 15:35:46 +0400

    Fxing cookies

--- app.js
+++ app.js
@@ -71,8 +71,8 @@ app.use(bodyParser.json());
 
 // configuring CORS
 const corsOptions = {
-  // origin: "https://modul-dictionary.herokuapp.com",
-  origin: "*",
+  origin: "https://modul-dictionary.herokuapp.com",
+  // origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
