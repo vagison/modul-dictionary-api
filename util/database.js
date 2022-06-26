42b5d426466e21dc0714a1342950637d899e23bd
unknown <vagison@gmail.com>
2022-06-26 23:45:56 +0400

    Fixing errors

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@
 const { Sequelize } = require('sequelize')
 
 // --- Creating the database connection
-const db = process.env.JAWSDB_URL & process.env.ENVIRONMENT === "PRODUCTION"
+const db = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_SCHEME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
       dialect: process.env.DB_DIALECT,
