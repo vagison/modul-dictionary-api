1f4fe822eb03f35962a571a5ef41ea828946ac86
unknown <vagison@gmail.com>
2022-06-26 23:52:41 +0400

    Fixing database connection file

--- util/database.js
+++ util/database.js
@@ -4,12 +4,13 @@
 const { Sequelize } = require('sequelize')
 
 // --- Creating the database connection
-const db = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_SCHEME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-      dialect: process.env.DB_DIALECT,
-      host: process.env.DB_HOST
-    })
+const db =
+  process.env.JAWSDB_URL && process.env.ENVIRONMENT === 'PRODUCTION'
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_SCHEME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST
+      })
 
 // --- Exporting the database connection
 module.exports = db
